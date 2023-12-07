1d618b5bc9db28fcc5b850337bd955fabcbefbce
zuiyu1998 <1542844298@qq.com>
2023-12-07 10:12:47 +0800

    feat: 添加dayjs local

--- ui/tauri/src/components/application/application_provider.tsx
+++ ui/tauri/src/components/application/application_provider.tsx
@@ -2,6 +2,7 @@ import { ConfigProvider } from 'antd';
 import { PropsWithChildren } from 'react';
 
 import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
 
 function ApplicationProvider(props: PropsWithChildren) {
   return <ConfigProvider locale={zhCN}>{props.children}</ConfigProvider>;
